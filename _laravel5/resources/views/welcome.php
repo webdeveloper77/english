49475938d4bac2c0964e940903fddff82b6b0cce
dspirin <dmiriy.spirin@gmail.com>
2015-11-04 10:36:44 +0300

    Cleaned Laravel

--- _laravel5/resources/views/welcome.php
+++ _laravel5/resources/views/welcome.php
@@ -41,7 +41,7 @@
 		<div class="container">
 			<div class="content">
 				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="quote">dadasdasd</div>
 			</div>
 		</div>
 	</body>
